8fa3a4f3f7f69eb9a9e49c4346e3bc60a09db96f
oooooconcac <boduynek210@gmail.com>
2026-05-17 13:18:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 // ==UserScript==
 // @name         ANAKU x LOVANLEO
 // @namespace    anaku-lovanleo-upto
-// @version      99.9
+// @version      100.2
 // @description  ANAKU x LOVANLEO
-// @require      https://raw.githubusercontent.com/oooooconcac/Cuttay-uptolink-/main/logic.js
+// @require      https://raw.githubusercontent.com/oooooconcac/Cuttay-uptolink-/main/logic.js?ver=1002
 // @author       ANAKU x LOVANLEO
 // @match        *://linkhuongdan.online/*
-// @grant        GM_xmlhttpRequest
-// @connect      uptolink.one
-// @connect      raw.githubusercontent.com
-// @connect      api.github.com
-// @connect      *
-// @run-at       document-end
+// @grant        none
+// @run-at       document-idle
 // ==/UserScript==
